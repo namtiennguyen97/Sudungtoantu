55b5fadd1bad243833567db081e0584f20afbeed
namtiennguyen <toilanam97@mail.com>
2019-08-01 09:15:44 +0700

    Su dung toan tu

---
+++ nhung.js
@@ -0,0 +1,14 @@
+
+let inputA ;
+let inputB;
+let area;
+inputA = Number ( prompt("Nhập chiều dài: "));
+inputB = Number (prompt("Nhap chiều rộng: "));
+document.write('Chiều dài đã nhập: ' + inputA);
+document.write("</br>");
+document.write("Chiều rộng đã nhập: " + inputB);
+document.write("</br>");
+area = inputA * inputB;
+document.write('Diện tích là: ' + area);
+
+
